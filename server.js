fac5b63baf00d46e1e7d90780850db98cb2cf611
JackCowling123 <jackcowling77@gmail.com>
2025-02-17 20:44:07 +0000

    changed port again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app'); // Imports Express app, where main functionality happens
 const mongoose = require('./config/db'); // Connect to noSQL DB, MongoDB. Setup is handed in db file.
 
-const { PORT = 9090 } = process.env; // Either accesses environment variables through the api secret key in the env file (to prevent unauthorised access) or  defauls to 5000, running locally
+const PORT = process.env.PORT || 4000; // Either accesses environment variables through the api secret key in the env file (to prevent unauthorised access) or  defauls to 4000, running locally
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`)); // Makes app listen for requests
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Makes app listen for requests
